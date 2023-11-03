d88f9347eeddb19316f9b6be50fbffb3dfc1a8ca
StefanoMela <stefano.mela25@gmail.com>
2023-11-03 16:24:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from 'axios';
+import {store} from './data/store.js'
+
+import ProjectList from './components/ProjectList.vue'
+
+export default {
+
+data() {
+  return {
+    store,
+  }
+},
+
+components: { ProjectList },
+
+methods: {
+
+  fetchProjects(){
+    axios.get(this.store.baseUrl + 'projects').then((response) => {
+
+      store.projects = response.data.data;
+
+    }
+  )
+
+},
+},
+
+created() {
+
+  this.fetchProjects();
+}
+};
+
+
+</script>
+
+<template>
+  <seciont class="container my-5 text-center">
+    <h1>Project List</h1>
+    <ProjectList v-for="project in store.projects" :project="project" />
+  </seciont>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {      
+    }
+  },
+
+  props: {
+
+    project: Object,
+
+  },
+};
+</script>
+
+<template>
+        <div class="row row-cols-3 g-4">
+            <div class="col">
+                    <div class="card-body">
+                        <h4>{{ project.title }}</h4>
+                    </div>
+            </div>
+        </div>
+</template>
+
+<style lang="scss" scoped></style>
